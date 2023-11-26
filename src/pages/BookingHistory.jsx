6080ff5a7b73e71cd765a0a1272dfe8a0f8f0dd0
Michael Tworek <m.tworek@student.fontys.nl>
2023-11-26 21:42:23 +0100

    j

--- src/pages/BookingHistory.jsx
+++ src/pages/BookingHistory.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import style from './css/BookingHistory.module.css'
 import BookingHistoryFilters from '../components/BookingHistoryComponents/BookingHistoryFilters'
 import BookingList from '../components/BookingHistoryComponents/BookingList'
-import BookingApi from '../api/BookingApi'
+import BookingAPI from '../api/BookingApi'
 import { useParkingGarage } from '../components/ParkingGarageContext';
 const BookingHistory = () => {
   const { filters } = useParkingGarage();
@@ -15,24 +15,15 @@ const BookingHistory = () => {
   
   useEffect(() => {
     const fetchFilteredBookings = async () => {
-        // Constructing the URL for fetching
-        const fetchUrl = `http://localhost:8080/bookings/filter?garageId=${filters.garageId}&service=${filters.service}&finished=${filters.finished}&ongoing=${filters.ongoing}`;
-        console.log("Fetching from URL:", fetchUrl);
-
         try {
-            // Fetching filtered bookings
-            const response = await fetch(fetchUrl, { headers: {
-              Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
-          });
+            // Consistent usage of BookingAPI
+            const data = await BookingAPI.getFilteredBookings(
+                filters.garageId, 
+                filters.service, 
+                filters.finished, 
+                filters.ongoing
+            );
 
-            // Check for a valid response
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            // Parsing the response as JSON
-            const data = await response.json();
             console.log('Data received:', data); // Debugging: Log the received data
 
             // Updating state with the fetched bookings
@@ -43,10 +34,7 @@ const BookingHistory = () => {
         }
     };
 
-    // Call the fetch function if the necessary filter (garageId) is set
-    if (filters.garageId) {
-        fetchFilteredBookings();
-    }
+    fetchFilteredBookings();
 }, [filters]);
   useEffect(() => {
     const sortedBookings = sortBookings(bookings, sortBy);
