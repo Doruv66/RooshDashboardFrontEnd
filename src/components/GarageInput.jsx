f2ef28dbf9b4575a2ee22e5b0a414633ef49a3d3
Roan Kessels <511636@student.fontys.nl>
2023-12-19 11:23:22 +0100

    Fixed the near disaster

--- src/components/GarageInput.jsx
+++ src/components/GarageInput.jsx
@@ -2,12 +2,24 @@ import React, {forwardRef, useEffect, useImperativeHandle, useRef, useState} fro
 import './GarageInput.css';
 import { useParkingGarage } from "./ParkingGarageContext";
 import ParkingGarageApi from '../api/ParkingGarageApi';
-import {Box, Button, ImageList, ImageListItem, Tab, Tabs, Typography} from '@mui/material';
+import {
+    Box,
+    Button,
+    FormControl,
+    FormGroup,
+    FormLabel,
+    ImageList,
+    ImageListItem, Popover,
+    Tab,
+    Tabs,
+    Typography
+} from '@mui/material';
 import {useNavigate} from "react-router-dom";
 import TextField from '@mui/material/TextField';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Grid from "@mui/material/Grid";
 export default function GarageInput(){
     const { parkingGarage, setParkingGarage } = useParkingGarage();
     const parkingGarageAttributes = ["name", "airport", "location", "travelTime", "travelDistance", "phoneNumber"];
@@ -95,6 +107,18 @@ export default function GarageInput(){
 
     const TabOneContent = forwardRef((props, ref) => {
         const [localValues, setLocalValues] = useState({});
+        const [anchorEl, setAnchorEl] = useState(null);
+
+        const handlePopoverOpen = (event) => {
+            setAnchorEl(event.currentTarget);
+        };
+
+        const handlePopoverClose = () => {
+            setAnchorEl(null);
+        };
+
+        const open = Boolean(anchorEl);
+
         useEffect(() => {
             setLocalValues(formValues);
         }, [formValues]);
@@ -137,16 +161,150 @@ export default function GarageInput(){
                 label = label + ' (in meters)';
             }
 
-            return (
-                <TextField
-                    name={attr}
-                    key={attr}
-                    label={label}
-                    className="textField"
-                    value={localValues[attr] || ''}
-                    onChange={(e) => handleLocalChange(attr, e.target.value)}
-                />
+            const CoordinatesGroup = () => (
+                <FormControl component="fieldset" className="coordinates-group" fullWidth>
+                    <FormLabel component="legend" sx={{ml:2}}>Coordinates</FormLabel>
+                    <FormGroup row sx={{ml:2}}>
+                        <TextField
+                            placeholder="Long"
+                            variant="outlined"
+                            sx={{backgroundColor: '#FFFFFF', width: '40%'}}
+                        />
+                        <TextField
+                            placeholder="Lat"
+                            variant="outlined"
+                            sx={{backgroundColor: '#FFFFFF', width: '40%'}}
+                        />
+                    </FormGroup>
+                </FormControl>
             );
+
+            const AddressGroup = () => (
+                <div className="address-details">
+                    <FormControl component="fieldset" className="address-group" fullWidth>
+                        <FormLabel component="legend" sx={{ml:2}}>Address Details</FormLabel>
+                        <FormGroup sx={{ml:2}}>
+                            <TextField
+                                placeholder="Street and Address"
+                                variant="outlined"
+                                margin="normal"
+                                sx={{backgroundColor: '#FFFFFF',width: '100%', mb:0}}
+                            />
+                            <TextField
+                                margin="normal"
+                                placeholder="Zip code"
+                                sx={{ mr: 8}}
+                            />
+                            <TextField
+                                margin="normal"
+                                placeholder="City"
+                            />
+                            <TextField
+                                placeholder="Country"
+                                variant="outlined"
+                                margin="normal"
+                                sx={{backgroundColor: '#FFFFFF', width: '100%'}}
+                            />
+                        </FormGroup>
+                    </FormControl>
+                </div>
+            );
+
+            return (
+                <React.Fragment key={attr}>
+                    {attr === 'airport' ? (
+                        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
+                            <Button
+                                aria-owns={open ? 'mouse-over-popover' : undefined}
+                                aria-haspopup="true"
+                                onClick={handlePopoverOpen}
+                                variant="contained"
+                                style={{ marginRight: '8px', flexShrink: 0, height: '56px', backgroundColor: '#FF9000' }}
+                            >
+                                Add Airport
+                            </Button>
+                            <TextField
+                                name={attr}
+                                label={label}
+                                value={localValues[attr] || ''}
+                                onChange={(e) => handleLocalChange(attr, e.target.value)}
+                                variant="outlined"
+                                style={{width:'70%'}}
+                            />
+                        </div>
+                    ) : (
+                        <TextField
+                            name={attr}
+                            label={label}
+                            value={localValues[attr] || ''}
+                            onChange={(e) => handleLocalChange(attr, e.target.value)}
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                        />
+                    )}
+                    {attr === 'airport' && (
+                        <Popover
+                            id="mouse-over-popover"
+                            open={open}
+                            anchorEl={anchorEl}
+                            onClose={handlePopoverClose}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            sx={{
+                                "& .MuiPaper-root": {
+                                    width: "auto",
+                                    maxWidth: "30%",
+                                }
+                            }}
+                        >
+                            <div className="form-grid">
+                                <Box sx={{ padding: 3, borderRadius: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2,'&.MuiBox-root': {
+                                        marginTop: '0px',}, }}>
+                                    <Typography variant="h6" gutterBottom>
+                                        Add a new Airport
+                                    </Typography>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12} md={12}>
+                                            <TextField
+                                                label="IATA Airport Code"
+                                                placeholder="Enter Code"
+                                                variant="outlined"
+                                                sx={{backgroundColor: '#FFFFFF',width: '100%', mt: 1, mb: 1}}
+                                            />
+                                            <TextField
+                                                label="Terminal Name"
+                                                placeholder="Enter Name"
+                                                variant="outlined"
+                                                sx={{backgroundColor: '#FFFFFF',width: '100%', mt: 1, mb: 1}}
+                                            />
+                                            <TextField
+                                                label="Airport Name"
+                                                placeholder="Enter Name"
+                                                variant="outlined"
+                                                sx={{backgroundColor: '#FFFFFF',width: '100%', mt: 1, mb: 2}}
+                                            />
+                                        </Grid>
+                                        <AddressGroup/>
+                                        <CoordinatesGroup/>
+                                        <div className="button-container">
+                                            <Button variant="contained" className="form-button" sx={{mr:2, ml:2, padding: '10px 40px'}}>Save</Button>
+                                            <Button variant="contained" style={{ backgroundColor: '#ef1846'}} sx={{padding: '10px 40px'}} className="form-button">Delete</Button>
+                                        </div>
+                                    </Grid>
+                                </Box>
+                            </div>
+                        </Popover>
+                    )}
+                </React.Fragment>
+            );
+
         });
 
         return (
@@ -162,39 +320,39 @@ export default function GarageInput(){
                     {!isNewParkingGarage && parkingGarage && (
                         <div className="crud-button-container">
                             <Button type="submit"
-                            variant="contained"
-                            sx={{
-                                width: 'max-content',
-                                margin: '5%',
-                                padding: "12px 20px",
-                                fontSize: "large",
-                                letterSpacing: "1px",
-                                textTransform: 'none',
-                                bgcolor: "#DA4A0C",
-                                '&:hover': {
-                                    bgcolor: '#e80',
-                                },
-                                borderRadius: '10px',
-                                marginLeft: '5%',
-                                }}>
+                                    variant="contained"
+                                    sx={{
+                                        width: 'max-content',
+                                        margin: '5%',
+                                        padding: "12px 20px",
+                                        fontSize: "large",
+                                        letterSpacing: "1px",
+                                        textTransform: 'none',
+                                        bgcolor: "#DA4A0C",
+                                        '&:hover': {
+                                            bgcolor: '#e80',
+                                        },
+                                        borderRadius: '10px',
+                                        marginLeft: '5%',
+                                    }}>
                                 Update parking garage
                             </Button>
                             <Button
                                 variant="contained"
                                 onClick={handleDeleteParkingGarage}
                                 sx={{
-                                width: 'max-content',
-                                margin: '5%',
-                                padding: "12px 20px",
-                                fontSize: "large",
-                                letterSpacing: "1px",
-                                textTransform: 'none',
-                                bgcolor: "#DA4A0C",
-                                '&:hover': {
-                                    bgcolor: '#e80',
-                                },
-                                borderRadius: '10px',
-                                marginLeft: '5%',
+                                    width: 'max-content',
+                                    margin: '5%',
+                                    padding: "12px 20px",
+                                    fontSize: "large",
+                                    letterSpacing: "1px",
+                                    textTransform: 'none',
+                                    bgcolor: "#DA4A0C",
+                                    '&:hover': {
+                                        bgcolor: '#e80',
+                                    },
+                                    borderRadius: '10px',
+                                    marginLeft: '5%',
                                 }}
                             >
                                 Delete parking garage
@@ -203,22 +361,22 @@ export default function GarageInput(){
                     )}
                     {isNewParkingGarage && (
                         <div className="crud-button-container">
-                            <Button type="submit" 
-                            variant="contained"
-                            sx={{
-                                width: 'max-content',
-                                margin: '5%',
-                                padding: "12px 20px",
-                                fontSize: "large",
-                                letterSpacing: "1px",
-                                textTransform: 'none',
-                                bgcolor: "#DA4A0C",
-                                '&:hover': {
-                                    bgcolor: '#e80',
-                                },
-                                borderRadius: '10px',
-                                marginLeft: '5%',
-                                }}>
+                            <Button type="submit"
+                                    variant="contained"
+                                    sx={{
+                                        width: 'max-content',
+                                        margin: '5%',
+                                        padding: "12px 20px",
+                                        fontSize: "large",
+                                        letterSpacing: "1px",
+                                        textTransform: 'none',
+                                        bgcolor: "#DA4A0C",
+                                        '&:hover': {
+                                            bgcolor: '#e80',
+                                        },
+                                        borderRadius: '10px',
+                                        marginLeft: '5%',
+                                    }}>
                                 Save new parking garage
                             </Button>
                         </div>
